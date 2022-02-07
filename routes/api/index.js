c1b9ae65a122f6fc2fa8c7970bb1e63d71b0520b
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2022-02-07 18:32:24 -0500

    post model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./touch post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
